e021e0895ce6ec499a676690116bc044813aac9f
Luca Manzon <manzonluca@gmail.com>
2021-05-03 21:32:51 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./navbar/Navbar.js";
+import FormField from "./formfield/FormField.js";
+import CoolButton from "./coolbutton/Coolbutton.js";
+import Signup from "./signup/signup";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+
+      <CoolButton isSmall isDanger className="button is-primary">
+        Button 1
+      </CoolButton>
+      <CoolButton isSmall isSuccess>
+        Button 2
+      </CoolButton>
+      <Signup />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
