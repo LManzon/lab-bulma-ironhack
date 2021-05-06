24bc9d9f434f5df918493ee26c8d5a1e3c7b7b8b
Luca Manzon <manzonluca@gmail.com>
2021-05-06 18:15:02 +0200

    final done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Navbar from "./navbar/Navbar.js";
 import FormField from "./formfield/FormField.js";
 import CoolButton from "./coolbutton/Coolbutton.js";
-import Signup from "./signup/signup";
+// import Signup from "./signup/signup";
 
 function App() {
   return (
@@ -24,22 +24,24 @@ function App() {
         Button 2
       </CoolButton>
       <Signup />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
 
 export default App;
+
+function Signup() {
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="haha" />
+      <FormField label="Email" type="email" placeholder="haha" />
+      <FormField label="Password" type="password" placeholder="haha" />
+
+      <CoolButton isSuccess isSmall>
+        {" "}
+        Submit
+      </CoolButton>
+    </div>
+  );
+}
--- src/coolbutton/Coolbutton.js
+++ src/coolbutton/Coolbutton.js
@@ -1,11 +1,57 @@
 import React from "react";
 
+const allPossibleClasses = {
+  isActive: "is-active",
+  isBlack: "is-black",
+  isCentered: "is-centered",
+  isDanger: "is-danger",
+  isDark: "is-dark",
+  isFocused: "is-focused",
+  isGrouped: "is-grouped",
+  isHovered: "is-hovered",
+  isInfo: "is-info",
+  isInverted: "is-inverted",
+  isLarge: "is-large",
+  isLight: "is-light",
+  isLink: "is-link",
+  isLoading: "is-loading",
+  isMedium: "is-medium",
+  isOutlined: "is-outlined",
+  isPrimary: "is-primary",
+  isRight: "is-right",
+  isRounded: "is-rounded",
+  isSelected: "is-selected",
+  isSmall: "is-small",
+  isStatic: "is-static",
+  isSuccess: "is-success",
+  isText: "is-text",
+  isWarning: "is-warning",
+  isWhite: "is-white",
+};
+
 function CoolButton(props) {
-  return (
-    <div>
-      <button className={props.class}>button</button>
-    </div>
-  );
+  const { children, className = " ", ...everyOtherPropsFromParent } = props;
+
+  let classes = `button is-rounded ${className} `;
+
+  for (const key in everyOtherPropsFromParent) {
+    if (allPossibleClasses[key]) {
+      classes += `${allPossibleClasses[key]} `;
+    }
+  }
+  // if (props.isSmall) {
+  //   classes += "is-small ";
+  // }
+
+  // if (props.isDanger) {
+  //   classes += "is-danger ";
+  // }
+
+  // if (props.isSuccess) {
+  //   classes += "is-success ";
+  // }
+
+  return <button className={classes}>{props.children}</button>;
 }
 
 export default CoolButton;
